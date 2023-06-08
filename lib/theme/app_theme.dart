711bbbe7960e89e617d979c9f054a39bc824121a
Shinex <653143454@qq.com>
2023-06-09 02:34:41 +0800

    更新主题样式

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,20 +3,21 @@ import 'package:flutter/services.dart';
 
 class AppTheme {
   ///MaterialApp主题配置
-  static final ThemeData themeData = ThemeData(
-    useMaterial3: true,
-    scaffoldBackgroundColor: Colors.grey.shade100,
-    appBarTheme: const AppBarTheme(
-      elevation: 0,
-      scrolledUnderElevation: 0,
-      centerTitle: true,
-      backgroundColor: Colors.white,
-      foregroundColor: Colors.black87,
-      titleTextStyle: TextStyle(fontSize: 20, color: Colors.black87),
-      systemOverlayStyle: SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarIconBrightness: Brightness.dark,
-      ),
-    ),
-  );
+  static get themeData => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey.shade100,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          scrolledUnderElevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Brightness.dark,
+          ),
+        ),
+      );
 }
