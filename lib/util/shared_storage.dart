d3339882d2100a0c863474a812b894640abdb33a
Shinex <653143454@qq.com>
2023-07-26 15:14:18 +0800

    优化代码

--- lib/util/shared_storage.dart
+++ lib/util/shared_storage.dart
@@ -1,4 +1,4 @@
-import 'package:shared_storage/shared_storage.dart';
+import 'package:shared_storage/shared_storage.dart' as storage;
 
 import 'package:huazhixia/config/config.dart';
 
@@ -6,12 +6,12 @@ import 'package:huazhixia/config/config.dart';
 class SharedStorage {
   ///检查指定目录是否已经授予
   static Future<bool> checkUriGrant(Uri directoryUri) async {
-    return await isPersistedUri(directoryUri);
+    return await storage.isPersistedUri(directoryUri);
   }
 
   ///使用Saf授予指定目录
   static Future<GrantUriState> grantDirectory(Uri directoryUri) async {
-    final grantUri = await openDocumentTree(initialUri: directoryUri);
+    final grantUri = await storage.openDocumentTree(initialUri: directoryUri);
 
     if (grantUri != null) {
       return grantUri == directoryUri
@@ -24,20 +24,21 @@ class SharedStorage {
 
   ///查看游戏目录内指定的文件是否存在
   static Future<bool> fileExist(String fileName) async {
-    final file = await findFile(UriConfig.mainUri, fileName);
+    final file = await storage.findFile(UriConfig.mainUri, fileName);
 
     return file != null ? true : false;
   }
 
   ///读取指定文件内容（需先判断文件是否存在，防止崩溃）
   static Future<String?> readFileContent(Uri fileUri) async {
-    final fileContent = await getDocumentContentAsString(fileUri);
+    final fileContent = await storage.getDocumentContentAsString(fileUri);
     return fileContent;
   }
 
   ///写入内容到指定文件（需先判断文件是否存在，防止崩溃）
   static Future<bool> writeFileContent(Uri fileUri, String content) async {
-    final fileWrite = await writeToFileAsString(fileUri, content: content);
+    final fileWrite =
+        await storage.writeToFileAsString(fileUri, content: content);
     if (fileWrite != null && fileWrite == true) {
       return true;
     }
@@ -46,7 +47,7 @@ class SharedStorage {
 
   ///在游戏目录内创建文件（需先判断文件是否存在，防止多生成几份备份文件）
   static Future<bool> createFile(String fileName, String content) async {
-    final fileCreate = await createFileAsString(
+    final fileCreate = await storage.createFileAsString(
       UriConfig.mainUri,
       mimeType: '',
       displayName: fileName,
@@ -57,7 +58,7 @@ class SharedStorage {
 
   ///删除文件（需先判断文件是否存在，防止崩溃）
   static Future<bool> deleteFile(Uri fileUri) async {
-    final fileDelete = await delete(fileUri);
+    final fileDelete = await storage.delete(fileUri);
     if (fileDelete != null && fileDelete == true) {
       return true;
     }
