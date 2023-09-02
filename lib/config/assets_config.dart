0cc45cb14b432dc4d3e9147dbe7b1bcd0039ff16
Shinex <653143454@qq.com>
2023-09-02 22:30:17 +0800

    更改assets里的文件夹名称、升级sp依赖

--- lib/config/assets_config.dart
+++ lib/config/assets_config.dart
@@ -1,26 +1,26 @@
 class AssetsConfig {
-  static const String appIcon = 'assets/icon/icon.png';
-  static const String lightning = 'assets/icon/lightning.png';
-  static const String arrow = 'assets/icon/arrow.png';
-  static const String modelImitate = 'assets/icon/model_imitate.png';
-  static const String permission = 'assets/icon/permission.png';
+  static const String appIcon = 'assets/icons/icon.png';
+  static const String lightning = 'assets/icons/lightning.png';
+  static const String arrow = 'assets/icons/arrow.png';
+  static const String modelImitate = 'assets/icons/model_imitate.png';
+  static const String permission = 'assets/icons/permission.png';
 
-  static const String quick = 'assets/icon/quick.png';
-  static const String power = 'assets/icon/power.png';
-  static const String other = 'assets/icon/other.png';
-  static const String restore = 'assets/icon/restore.png';
-  static const String diversify = 'assets/icon/diversify.png';
-  static const String model = 'assets/icon/model.png';
-  static const String wideAngle = 'assets/icon/wide_angle.png';
-  static const String random = 'assets/icon/random.png';
+  static const String quick = 'assets/icons/quick.png';
+  static const String power = 'assets/icons/power.png';
+  static const String other = 'assets/icons/other.png';
+  static const String restore = 'assets/icons/restore.png';
+  static const String diversify = 'assets/icons/diversify.png';
+  static const String model = 'assets/icons/model.png';
+  static const String wideAngle = 'assets/icons/wide_angle.png';
+  static const String random = 'assets/icons/random.png';
 
-  static const String vip = 'assets/icon/vip.png';
-  static const String theme = 'assets/icon/theme.png';
-  static const String font = 'assets/icon/font.png';
-  static const String useHelp = 'assets/icon/use_help.png';
-  static const String download = 'assets/icon/download.png';
-  static const String contactAuthor = 'assets/icon/contact_author.png';
-  static const String share = 'assets/icon/share.png';
-  static const String checkUpdate = 'assets/icon/check_update.png';
-  static const String about = 'assets/icon/about.png';
+  static const String vip = 'assets/icons/vip.png';
+  static const String theme = 'assets/icons/theme.png';
+  static const String font = 'assets/icons/font.png';
+  static const String useHelp = 'assets/icons/use_help.png';
+  static const String download = 'assets/icons/download.png';
+  static const String contactAuthor = 'assets/icons/contact_author.png';
+  static const String share = 'assets/icons/share.png';
+  static const String checkUpdate = 'assets/icons/check_update.png';
+  static const String about = 'assets/icons/about.png';
 }
--- lib/config/file_config.dart
+++ lib/config/file_config.dart
@@ -1,96 +1,96 @@
 // 画质修改等文件的配置
 class FileConfig {
   /// 超高音质
-  static const String tqSuperHigh = 'assets/file/tq_superhigh.ini';
+  static const String tqSuperHigh = 'assets/files/tq_superhigh.ini';
 
   /// 解锁画质帧率
-  static const String dlPq120Fps = 'assets/file/dl_pq120fps.ini';
+  static const String dlPq120Fps = 'assets/files/dl_pq120fps.ini';
 
   /// 快捷修改
   static const List quickFile = [
-    'assets/file/kjxg/kjxg_1.ini',
-    'assets/file/kjxg/kjxg_2.ini',
-    'assets/file/kjxg/kjxg_3.ini',
-    'assets/file/kjxg/kjxg_4.ini',
-    'assets/file/kjxg/kjxg_5.ini',
-    'assets/file/kjxg/kjxg_6.ini'
+    'assets/files/kjxg/kjxg_1.ini',
+    'assets/files/kjxg/kjxg_2.ini',
+    'assets/files/kjxg/kjxg_3.ini',
+    'assets/files/kjxg/kjxg_4.ini',
+    'assets/files/kjxg/kjxg_5.ini',
+    'assets/files/kjxg/kjxg_6.ini'
   ];
 
   /// 专属修改
   static const List exclusiveFile = [
-    'assets/file/zsxg/zsxg_1.ini',
-    'assets/file/zsxg/zsxg_2.ini',
-    'assets/file/zsxg/zsxg_3.ini',
-    'assets/file/zsxg/zsxg_4.ini',
-    'assets/file/zsxg/zsxg_5.ini',
-    'assets/file/zsxg/zsxg_6.ini',
-    'assets/file/zsxg/zsxg_7.ini',
-    'assets/file/zsxg/zsxg_8.ini',
-    'assets/file/zsxg/zsxg_9.ini',
-    'assets/file/zsxg/zsxg_10.ini',
-    'assets/file/zsxg/zsxg_11.ini',
-    'assets/file/zsxg/zsxg_12.ini'
+    'assets/files/zsxg/zsxg_1.ini',
+    'assets/files/zsxg/zsxg_2.ini',
+    'assets/files/zsxg/zsxg_3.ini',
+    'assets/files/zsxg/zsxg_4.ini',
+    'assets/files/zsxg/zsxg_5.ini',
+    'assets/files/zsxg/zsxg_6.ini',
+    'assets/files/zsxg/zsxg_7.ini',
+    'assets/files/zsxg/zsxg_8.ini',
+    'assets/files/zsxg/zsxg_9.ini',
+    'assets/files/zsxg/zsxg_10.ini',
+    'assets/files/zsxg/zsxg_11.ini',
+    'assets/files/zsxg/zsxg_12.ini'
   ];
 
   /// 高优化修改
   static const List highoptiFile = [
-    'assets/file/gyhxg/gyhxg_1.ini',
-    'assets/file/gyhxg/gyhxg_2.ini',
-    'assets/file/gyhxg/gyhxg_3.ini',
-    'assets/file/gyhxg/gyhxg_4.ini',
-    'assets/file/gyhxg/gyhxg_5.ini',
-    'assets/file/gyhxg/gyhxg_6.ini',
-    'assets/file/gyhxg/gyhxg_7.ini',
-    'assets/file/gyhxg/gyhxg_8.ini',
-    'assets/file/gyhxg/gyhxg_9.ini',
-    'assets/file/gyhxg/gyhxg_10.ini',
-    'assets/file/gyhxg/gyhxg_11.ini',
-    'assets/file/gyhxg/gyhxg_12.ini'
+    'assets/files/gyhxg/gyhxg_1.ini',
+    'assets/files/gyhxg/gyhxg_2.ini',
+    'assets/files/gyhxg/gyhxg_3.ini',
+    'assets/files/gyhxg/gyhxg_4.ini',
+    'assets/files/gyhxg/gyhxg_5.ini',
+    'assets/files/gyhxg/gyhxg_6.ini',
+    'assets/files/gyhxg/gyhxg_7.ini',
+    'assets/files/gyhxg/gyhxg_8.ini',
+    'assets/files/gyhxg/gyhxg_9.ini',
+    'assets/files/gyhxg/gyhxg_10.ini',
+    'assets/files/gyhxg/gyhxg_11.ini',
+    'assets/files/gyhxg/gyhxg_12.ini'
   ];
 
   /// 低端机
   static const List lowModelFile = [
-    'assets/file/ddj/ddj_1.ini',
-    'assets/file/ddj/ddj_2.ini',
-    'assets/file/ddj/ddj_3.ini',
-    'assets/file/ddj/ddj_4.ini',
-    'assets/file/ddj/ddj_5.ini',
-    'assets/file/ddj/ddj_6.ini',
-    'assets/file/ddj/ddj_7.ini',
-    'assets/file/ddj/ddj_8.ini',
-    'assets/file/ddj/ddj_9.ini',
-    'assets/file/ddj/ddj_10.ini',
-    'assets/file/ddj/ddj_11.ini',
-    'assets/file/ddj/ddj_12.ini'
+    'assets/files/ddj/ddj_1.ini',
+    'assets/files/ddj/ddj_2.ini',
+    'assets/files/ddj/ddj_3.ini',
+    'assets/files/ddj/ddj_4.ini',
+    'assets/files/ddj/ddj_5.ini',
+    'assets/files/ddj/ddj_6.ini',
+    'assets/files/ddj/ddj_7.ini',
+    'assets/files/ddj/ddj_8.ini',
+    'assets/files/ddj/ddj_9.ini',
+    'assets/files/ddj/ddj_10.ini',
+    'assets/files/ddj/ddj_11.ini',
+    'assets/files/ddj/ddj_12.ini'
   ];
 
   /// 中端机
   static const List mediumModelFile = [
-    'assets/file/zdj/zdj_1.ini',
-    'assets/file/zdj/zdj_2.ini',
-    'assets/file/zdj/zdj_3.ini',
-    'assets/file/zdj/zdj_4.ini',
-    'assets/file/zdj/zdj_5.ini',
-    'assets/file/zdj/zdj_6.ini',
-    'assets/file/zdj/zdj_7.ini',
-    'assets/file/zdj/zdj_8.ini',
-    'assets/file/zdj/zdj_9.ini',
-    'assets/file/zdj/zdj_10.ini',
-    'assets/file/zdj/zdj_11.ini'
+    'assets/files/zdj/zdj_1.ini',
+    'assets/files/zdj/zdj_2.ini',
+    'assets/files/zdj/zdj_3.ini',
+    'assets/files/zdj/zdj_4.ini',
+    'assets/files/zdj/zdj_5.ini',
+    'assets/files/zdj/zdj_6.ini',
+    'assets/files/zdj/zdj_7.ini',
+    'assets/files/zdj/zdj_8.ini',
+    'assets/files/zdj/zdj_9.ini',
+    'assets/files/zdj/zdj_10.ini',
+    'assets/files/zdj/zdj_11.ini'
   ];
 
   /// 高端机
   static const List highModelFile = [
-    'assets/file/gdj/gdj_1.ini',
-    'assets/file/gdj/gdj_2.ini',
-    'assets/file/gdj/gdj_3.ini',
-    'assets/file/gdj/gdj_4.ini',
-    'assets/file/gdj/gdj_5.ini',
-    'assets/file/gdj/gdj_6.ini',
-    'assets/file/gdj/gdj_7.ini',
-    'assets/file/gdj/gdj_8.ini',
-    'assets/file/gdj/gdj_9.ini',
-    'assets/file/gdj/gdj_10.ini'
+    'assets/files/gdj/gdj_1.ini',
+    'assets/files/gdj/gdj_2.ini',
+    'assets/files/gdj/gdj_3.ini',
+    'assets/files/gdj/gdj_4.ini',
+    'assets/files/gdj/gdj_5.ini',
+    'assets/files/gdj/gdj_6.ini',
+    'assets/files/gdj/gdj_7.ini',
+    'assets/files/gdj/gdj_8.ini',
+    'assets/files/gdj/gdj_9.ini',
+    'assets/files/gdj/gdj_10.ini'
   ];
 
   /// 所有画质修改文件集合
